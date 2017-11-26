677bf1c42b76d224bec61611c84d6d8903b10109
Kyle Bradford <kfoxb@kfoxb.com>
2017-11-26 16:44:11 -0700

    (feat) create Login component and add Google Sign in image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Login from './Login';
 
-export default class app extends React.Component {
+export default class App extends React.Component {
   render() {
-    return (<h1>Hi World</h1>);
+    return (
+      <Login />
+    );
   }
 }
