b28f6b4d2947b3688aa83a8bfdbe507278bf6e78
Kyle Bradford <kfoxb@kfoxb.com>
2017-11-25 23:39:40 -0700

    (feat) setup React and webpack dev server

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: require.resolve('babel-loader'),
+          options: {
+            cacheDirectory: true
+          }
+        }
+      }
+    ]
+  }
+};
+
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class app extends React.Component {
+  render() {
+    return (<h1>Hi World</h1>);
+  }
+}
